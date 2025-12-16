f77ec9a59dafed6d8f73769bccb8b2fa6d723f00
jessicamarq <jessicamaq@gmail.com>
2025-12-16 01:17:32 -0300

    adding exercise 36

--- desafio36.py
+++ desafio36.py
@@ -1,1 +1,9 @@
-print('isso é um teste')
+valorCasa = float(input('Digite o valor da casa: '))
+valorSalario = float(input('Digite o valor do salário do comprador: '))
+tempo = float(input('Digite em quantos anos pretende pagar: '))
+prestacao = valorCasa/(tempo*12)
+if (prestacao <= valorSalario*0.30):
+  print(f'O valor da prestação é R${prestacao:.2f} e seu empréstimo foi aprovado!')
+else:
+  print('O valor da prestação excede %d%% do seu salário.' % 30)
+  print('Empréstimo negado!')
