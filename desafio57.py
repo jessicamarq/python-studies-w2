8971743d292cd7bf3571018af068c05b450bf0e8
jessicamarq <jessicamaq@gmail.com>
2025-12-29 19:10:42 -0300

    2 more exercises

--- desafio57.py
+++ desafio57.py
@@ -1,2 +1,5 @@
 #Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F' caso esteja errado, peça a digitação novamente até ter um valor correto
-
+sexo = str(input("Qual o sexo? [M/F]? ")).upper().strip()[0]
+while (sexo != 'M') and (sexo != 'F'):
+  sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).upper().strip()[0]
+print(f'Sexo {sexo} registrado com sucesso!')
--- desafio58.py
+++ desafio58.py
@@ -1,2 +1,19 @@
 #melhore o jogo do desafio 28 onde o computador vai pensar em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer
 
+from random import randint
+sorteio = randint(0,10)
+print('Sou seu computador e estou pensando em um número de 0 a 10. ')
+num = int(input('Tente adivinhar... Qual seu palpite? '))
+cont = 1
+while num != sorteio:
+  if num < 0 or num > 10:
+    print('Opção inválida!')
+    num = int(input('Tente de novo com um número válido: '))
+    continue
+  cont+=1
+  if num < sorteio:
+    print('Você errou! Tente um número maior...')
+  else:
+    print('Você errou! Tente um número menor...')
+  num = int(input('Mais uma vez: '))
+print(f'Você acertou! Estava pensando no número {num} e você levou {cont} tentativas para acertar!')  
